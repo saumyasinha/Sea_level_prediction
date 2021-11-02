correct normalizing monthly values

ModuleLearning/ModuleCNN/Model.py
@@ -88,68 +88,68 @@ class FullyConvNet(nn.Module):
 
     # #     # smaller FCN
     #     self.encoder = nn.Sequential(
-    #         nn.Conv2d(dim_channels, 16, (3,3), stride=2, padding = 1),  #45*23
+    #         nn.Conv2d(dim_channels, 16, (3,3), stride=(2,2), padding = (1,1)),  #45*23
     #        # nn.BatchNorm2d(16),
     #         nn.ReLU(),
-    #         nn.Conv2d(16, 32,(3,3),stride=2,  padding = 1),#23*12
+    #         nn.Conv2d(16, 32,(3,3),stride=(2,2),  padding = (1,1)),#23*12
     #        # nn.BatchNorm2d(32),
     #         nn.ReLU(),
     #       #  nn.Dropout(0.3),
-    #         nn.Conv2d(32, 64, (3,3), stride =2, padding = 1), #12*6
+    #         nn.Conv2d(32, 64, (3,3), stride =(2,2), padding = (1,1)), #12*6
     #      #   nn.BatchNorm2d(64),
     #         nn.ReLU())
     #        # #nn.Dropout(0.3))
     #
     #     self.decoder = nn.Sequential(
     #
-    #         nn.ConvTranspose2d(64, 32, (3,2), stride = 2, padding = (1,0)), #23*12
+    #         nn.ConvTranspose2d(64, 32, (3,3), stride = (2,2), padding = (0,1)), #23*12
     #         # nn.BatchNorm2d(32),
     #         nn.ReLU(),
     #         # nn.Dropout(0.3),
-    #         nn.ConvTranspose2d(32, 16,(3,3), stride=2, padding = 1), #45*23
+    #         nn.ConvTranspose2d(32, 16,(3,3), stride=(2,2), padding = (1,1)), #45*23
     #         # nn.BatchNorm2d(16),
     #         nn.ReLU(),
     #         ##nn.Dropout(0.3),
-    #         nn.ConvTranspose2d(16, last_channel_size, (2, 3), stride=2, padding = (0,1))) #90*45
-    #
-    #     # ## SMALL fcn version2
-    #     # self.encoder = nn.Sequential(
-    #     #     nn.Conv2d(dim_channels, 32, (3, 3), stride=2, padding=1),  # 45*23
-    #     #     nn.BatchNorm2d(32),
-    #     #     nn.ReLU(),
-    #     #     nn.Conv2d(32, 64, (3, 3), stride=2, padding=1),  # 23*12
-    #     #     nn.BatchNorm2d(64),
-    #     #     nn.ReLU(),
-    #     #     nn.Conv2d(64, 128, (3, 3), stride=2, padding=1),  # 12*6
-    #     #     nn.BatchNorm2d(128),
-    #     #     nn.ReLU(),
-    #     #     nn.Dropout(0.3)
-    #     # )
-    #     #
-    #     # self.decoder = nn.Sequential(
-    #     #     nn.ConvTranspose2d(128, 64, (3,2), stride = 2, padding = (1,0)), #23*12
-    #     #     nn.BatchNorm2d(64),
-    #     #     nn.ReLU(),
-    #     #     nn.Dropout(0.3),
-    #     #     nn.ConvTranspose2d(64, 32,(3,3), stride=2, padding = 1), #45*23
-    #     #     nn.BatchNorm2d(32),
-    #     #     nn.ReLU(),
-    #     #     nn.ConvTranspose2d(32, last_channel_size, (2, 3), stride=2, padding = (0,1)) #90*45
-    #     #
-    #     #     # nn.Sigmoid(),
-    #     # )
-    #     #
-    #
-    #     self.apply(weights_init)
-    #
-    #
-    #
+    #         nn.ConvTranspose2d(16, last_channel_size, (2, 4), stride=(2,2), padding = (1,0))) #90*45
+    # #
+    # #     # ## SMALL fcn version2
+    # #     # self.encoder = nn.Sequential(
+    # #     #     nn.Conv2d(dim_channels, 32, (3, 3), stride=2, padding=1),  # 45*23
+    # #     #     nn.BatchNorm2d(32),
+    # #     #     nn.ReLU(),
+    # #     #     nn.Conv2d(32, 64, (3, 3), stride=2, padding=1),  # 23*12
+    # #     #     nn.BatchNorm2d(64),
+    # #     #     nn.ReLU(),
+    # #     #     nn.Conv2d(64, 128, (3, 3), stride=2, padding=1),  # 12*6
+    # #     #     nn.BatchNorm2d(128),
+    # #     #     nn.ReLU(),
+    # #     #     nn.Dropout(0.3)
+    # #     # )
+    # #     #
+    # #     # self.decoder = nn.Sequential(
+    # #     #     nn.ConvTranspose2d(128, 64, (3,2), stride = 2, padding = (1,0)), #23*12
+    # #     #     nn.BatchNorm2d(64),
+    # #     #     nn.ReLU(),
+    # #     #     nn.Dropout(0.3),
+    # #     #     nn.ConvTranspose2d(64, 32,(3,3), stride=2, padding = 1), #45*23
+    # #     #     nn.BatchNorm2d(32),
+    # #     #     nn.ReLU(),
+    # #     #     nn.ConvTranspose2d(32, last_channel_size, (2, 3), stride=2, padding = (0,1)) #90*45
+    # #     #
+    # #     #     # nn.Sigmoid(),
+    # #     # )
+    # #     #
+    # #
+    # #     self.apply(weights_init)
+    # #
+    # #
+    # #
     # def forward(self, x):
     #     x = self.encoder(x)
-    #   #  # print(x.shape)
+    #     # print(x.shape)
     #     x = self.decoder(x)
-    #    # # print(x.shape)
-    #     x = torch.squeeze(x)
+    #     print(x.shape)
+    #     # x = torch.squeeze(x)
     #    # # print(x.shape)
     #     return x
 
@@ -173,11 +173,11 @@ def trainBatchwise(trainX, trainY, validX,
 
     parallel = False
     if train_on_gpu:
-      #  if torch.cuda.device_count() > 1:
+        if torch.cuda.device_count() > 1:
            # print("Let's use", torch.cuda.device_count(), "GPUs!")
 
-    #        basic_forecaster = nn.DataParallel(basic_forecaster)
-     #       parallel = True
+           basic_forecaster = nn.DataParallel(basic_forecaster)
+           parallel = True
 
 
         basic_forecaster = basic_forecaster.cuda()
@@ -185,7 +185,7 @@ def trainBatchwise(trainX, trainY, validX,
 
     print(basic_forecaster)
 
-    optimizer = torch.optim.Adam(basic_forecaster.parameters(), lr=lr,betas=(0.9, 0.999), eps=1e-08) #, weight_decay = 1e-5)
+    optimizer = torch.optim.Adam(basic_forecaster.parameters(), lr=lr,betas=(0.9, 0.999), eps=1e-08, weight_decay = 1e-5)
     # scheduler = StepLR(optimizer, step_size=25, gamma=0.1)
     # criterion = torch.nn.MSELoss()
     # criterion = nn.L1Loss()

ModuleLearning/ModuleCNN/train.py
@@ -11,24 +11,24 @@ from ModuleLearning import eval
 def get_target_mask(y):
     # print(y[:5,0,0])
     missing_val = 1e+36
-    mask = (y != missing_val)
+    mask = ~np.isnan(y)#(y != missing_val)
     print("mask",mask.shape, y[mask].max(),y[mask].min())
     # print("num of ocean pixels: ", mask.sum())
-    # y[y == missing_val] = 0
+    y[np.isnan(y)] = 0
     return y, mask
 
 def basic_CNN_train(X_train, y_train, X_valid, y_valid, n_features, n_timesteps, epochs, batch_size, learning_rate, folder_saving, model_saved, quantile, alphas, n_predictions =1):
     valid = True
     outputs_quantile = len(alphas)
 
-    X_train, y_train = torch.from_numpy(X_train), torch.from_numpy(y_train)
-    X_valid, y_valid = torch.from_numpy(X_valid), torch.from_numpy(y_valid)
+    y_train, train_mask = get_target_mask(y_train)
+    y_valid, valid_mask = get_target_mask(y_valid)
+
+    X_train, y_train, train_mask = torch.from_numpy(X_train), torch.from_numpy(y_train), torch.from_numpy(train_mask)
+    X_valid, y_valid, valid_mask = torch.from_numpy(X_valid), torch.from_numpy(y_valid), torch.from_numpy(valid_mask)
 
     X_train = X_train.permute(0,3, 1, 2)
     X_valid = X_valid.permute(0,3, 1, 2)
-
-    y_train, train_mask = get_target_mask(y_train)
-    y_valid, valid_mask = get_target_mask(y_valid)
 
     train_loss, valid_loss = trainBatchwise(X_train, y_train, X_valid, y_valid, train_mask, valid_mask,
                                             n_predictions, n_features, n_timesteps, epochs, batch_size, learning_rate, folder_saving, model_saved, quantile,

ModuleLearning/ModuleCNN/unet.py
@@ -35,19 +35,34 @@ class UNet(nn.Module):
 
         # self.conv_last = nn.Conv2d(16, last_channel_size, 1)
 
-        self.dconv_down1 = double_conv(dim_channels, 16, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 0), padding_2=(1, 0))
+        self.dconv_down1 = double_conv(dim_channels, 16, kernel_1=(5, 3), kernel_2=(5, 3), padding_1=(0, 0), padding_2=(0, 0))
         self.dconv_down2 = double_conv(16, 32, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 1), padding_2=(1, 1))
         self.dconv_down3 = double_conv(32, 64, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 1), padding_2=(1, 1))
         # self.dconv_down4 = double_conv(256, 512, kernel=(3, 3), padding=(1, 1))
 
-        self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.maxpool = nn.MaxPool2d(4)
+        self.upsample = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
 
         # self.dconv_up3 = double_conv(256 + 512, 256, kernel=(3, 3), padding=(1, 1))
         self.dconv_up2 = double_conv(32 + 64, 32, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 1), padding_2=(1, 1))
-        self.dconv_up1 = double_conv(16 + 32, 16, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 2), padding_2=(1, 2))
+        self.dconv_up1 = double_conv(16 + 32, 16, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(3, 2), padding_2=(3, 2))
 
         self.conv_last = nn.Conv2d(16, last_channel_size, 1)
+
+        # self.dconv_down1 = double_conv(dim_channels, 16, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 0),
+        #                                padding_2=(1, 0))
+        # self.dconv_down2 = double_conv(16, 32, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 1), padding_2=(1, 1))
+        # self.dconv_down3 = double_conv(32, 64, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 1), padding_2=(1, 1))
+        # # self.dconv_down4 = double_conv(256, 512, kernel=(3, 3), padding=(1, 1))
+        #
+        # self.maxpool = nn.MaxPool2d(2)
+        # self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        #
+        # # self.dconv_up3 = double_conv(256 + 512, 256, kernel=(3, 3), padding=(1, 1))
+        # self.dconv_up2 = double_conv(32 + 64, 32, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 1), padding_2=(1, 1))
+        # self.dconv_up1 = double_conv(16 + 32, 16, kernel_1=(3, 3), kernel_2=(3, 3), padding_1=(1, 2), padding_2=(1, 2))
+        #
+        # self.conv_last = nn.Conv2d(16, last_channel_size, 1)
 
     def forward(self, x):
         # conv1 = self.dconv_down1(x)
@@ -86,22 +101,22 @@ class UNet(nn.Module):
         # #print("conv last",out.shape)
         # return out
         conv1 = self.dconv_down1(x)
-        # print("down1",conv1.shape)
+        #print("down1",conv1.shape)
         x = self.maxpool(conv1)
-        # print("maxpool",x.shape)
+        #print("maxpool",x.shape)
         conv2 = self.dconv_down2(x)
-        # print("down2", conv2.shape)
+        #print("down2", conv2.shape)
         x = self.maxpool(conv2)
 
-        # print("maxpool",x.shape)
+        #print("maxpool",x.shape)
         x = self.dconv_down3(x)
-        # print("down3", conv3.shape)
+        #print("down3", x.shape)
         #x = self.maxpool(conv3)
         #print("maxpool",x.shape)
         #x = self.dconv_down4(x)
         #print("down4",x.shape)
         x = self.upsample(x)
-        # print("upsample", x.shape)
+        #print("upsample", x.shape)
         #x = torch.cat([x, conv3], dim=1)
         #print("concat",x.shape)
         #x = self.dconv_up3(x)
@@ -109,15 +124,15 @@ class UNet(nn.Module):
         #x = self.upsample(x)
         #print("upsample", x.shape)
         x = torch.cat([x, conv2], dim=1)
-        # print("concat",x.shape)
+        #print("concat",x.shape)
         x = self.dconv_up2(x)
-        # print("up2", x.shape)
+        #print("up2", x.shape)
         x = self.upsample(x)
-        # print("upsample", x.shape)
+       # print("upsample", x.shape)
         x = torch.cat([x, conv1], dim=1)
-        # print("concat",x.shape)
+        #print("concat",x.shape)
         x = self.dconv_up1(x)
-        # print("up1", x.shape)
+        #print("up1", x.shape)
         out = self.conv_last(x)
-        # print("conv last",out.shape)
+        #print("conv last",out.shape)
         return out

ModuleLearning/preprocessing.py
@@ -4,8 +4,9 @@ import numpy as np
 def remove_land_values(xr):
 
     missing_val = 1e+36
-    print(np.max(xr), np.min(xr))
-    xr[xr == missing_val] = 0
+    print(np.nanmax(xr), np.nanmin(xr))
+    # xr[xr == missing_val] = 0
+    xr[np.isnan(xr)] = 0
     print(np.max(xr),np.min(xr))
     return xr
 
@@ -85,7 +86,11 @@ def create_train_test_split(model, path_1850_to_2014, path_2015_to_2100, train_s
     test = full_array[:, :, test_start_index-n_prev_months: test_end_index]
     print(test_start_index, test_end_index, test.shape)
 
-    print(np.min(train), np.max(train), np.min(test), np.max(test), np.isnan(train).sum(), np.isnan(test).sum())
+    ##masking land values with np.nan
+    train[train == 1e+36] = np.nan
+    test[test == 1e+36] = np.nan
+
+    print(np.nanmin(train), np.nanmax(train), np.nanmin(test), np.nanmax(test), np.isnan(train).sum(), np.isnan(test).sum())
     return train, test
 
 def convert_month_to_years(X):
@@ -97,7 +102,7 @@ def convert_month_to_years(X):
     for yr in range(0, n_years):
         X_sub = X[:,:,i:i + 12]
         annual_X = np.mean(X_sub, axis=2)
-        annual_X[annual_X>1e+36]=1e+36
+        # annual_X[annual_X>1e+36]=1e+36
         # print(annual_X.shape)
         X_yrs.append(annual_X)
         i = i + 12
@@ -151,9 +156,8 @@ def normalize_from_train(X_train, X_test):
         X_sub = X_train[:, :, indices_month]
         print(X_sub.shape)
         avg_for_month = np.mean(X_sub, axis=2)
-        avg_for_month[avg_for_month > 1e+36] = 1e+36
         print(avg_for_month.shape, avg_for_month[:,:,np.newaxis].shape)
-        X_train[:,:,indices_month] = X_train[:,:,indices_month]  - avg_for_month[:,:,np.newaxis]
+        X_train[:,:,indices_month] = X_train[:,:,indices_month] - avg_for_month[:,:,np.newaxis]
 
         n_months_test = X_test.shape[2]
         indices_month_test = [i for i in range(n_months_test) if i%12==month]

script_learn_predictions_with_CNN.py
@@ -45,7 +45,7 @@ n_prev_months = 12 #12
 yearly = False
 
 
-batch_size = 8
+batch_size = 4
 epochs = 200
 lr = 1e-4
 
@@ -64,17 +64,10 @@ def main():
         # np.save(path_data_fr+ model + "/"+"train_for_"+str(train_start_year)+"-"+str(train_end_year)+".npy", train)
         # np.save(path_data_fr+ model + "/"+"test_for_" + str(test_start_year) + "-" + str(test_end_year) + ".npy", test)
 
-        train[train != 1e+36] = train[train != 1e+36] / 100
-        test[test != 1e+36] = test[test != 1e+36] / 100
+        train = train / 100
+        test = test / 100
 
         X_train, y_train, X_test, y_test = preprocessing.create_labels(train, test, lead_years,n_prev_months)
-
-        ## remove land values
-        X_train = preprocessing.remove_land_values(X_train)
-        X_test = preprocessing.remove_land_values(X_test)
-
-        ## splitting train into train+valid later
-        split_index = 30 if yearly else 120*3
 
         ##if working with years instead of months
         if yearly:
@@ -83,6 +76,10 @@ def main():
         else:
             n_prev_times = n_prev_months
             X_train, X_test = preprocessing.normalize_from_train(X_train, X_test)
+
+        ## remove land values
+        X_train = preprocessing.remove_land_values(X_train)
+        X_test = preprocessing.remove_land_values(X_test)
 
         ## add previous timestep values
         X_train = preprocessing.include_prev_timesteps(X_train, n_prev_times)
@@ -100,6 +97,8 @@ def main():
         # X_train, X_valid, y_train, y_valid = train_test_split(
         #     X_train, y_train, test_size=0.2, random_state=42)
 
+        ## splitting train into train+valid
+        split_index = 30 if yearly else 120 * 3
 
         train_valid_split_index = len(X_train) - split_index #2*120 #keeping later 10 years for validation
         X,y = X_train,y_train
@@ -110,7 +109,6 @@ def main():
 
         print("train/valid sizes: ", len(X_train), " ", len(X_valid))
 
-        print(np.min(X_train),np.min(y_train),np.min(X_test), np.min(y_test))
         # X_train, X_valid, X_test = preprocessing.normalize_from_train(X_train,X_valid, X_test)
 
         # X_train_w_patches,y_train_w_patches = preprocessing.get_image_patches(X_train,y_train)
@@ -129,17 +127,17 @@ def main():
         #
         # y_valid_pred = np.load(folder_saving+"/valid_predictions.npy")
         # print(y_valid_pred.shape)
-        #
-        # yr_JAN2014 = y_valid[-7*12]
-        # yr_JAN2014_pred =y_valid_pred[-7*12]
-        # eval.plot(yr_JAN2014,folder_saving, "model_JAN2014_sla")
-        # eval.plot(yr_JAN2014_pred, folder_saving,"predicted_JAN2014_sla")
         # #
+        # # yr_JAN2014 = y_valid[-7*12]
+        # # yr_JAN2014_pred =y_valid_pred[-7*12]
+        # # eval.plot(yr_JAN2014,folder_saving, "model_JAN2014_sla")
+        # # eval.plot(yr_JAN2014_pred, folder_saving,"predicted_JAN2014_sla")
+        # # #
         # trend = eval.fit_trend(y_valid_pred, valid_mask, yearly=yearly)
-        # eval.plot(trend, folder_saving, "valid_trend_2001-2020", trend=True)
+        # eval.plot(trend, folder_saving, "valid_trend_1991-2020", trend=True)
         # # y_valid_wo_patches, valid_mask = train_cnn.get_target_mask(y_valid)
         # trend = eval.fit_trend(y_valid, valid_mask, yearly=yearly)
-        # eval.plot(trend, folder_saving, "model_trend_2001-2020", trend=True)
+        # eval.plot(trend, folder_saving, "model_trend_1991-2020", trend=True)
 
         # yr_2014 = y_valid[-1]
         # yr_2014_pred = y_valid_pred[-1]